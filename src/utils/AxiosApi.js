e6edd88ad8979133e8c33094ed24e62106ea6a88
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-09-02 23:38:50 +0200

    Changed headers in axios

--- src/utils/AxiosApi.js
+++ src/utils/AxiosApi.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 const axiosApi = axios.create({
   baseURL: process.env.REACT_APP_APIURL,
-  timeout: 10000,
   withCredentials: true,
+
   headers: {
-    'Content-Type': 'application/json'
+    "Access-Control-Allow-Origin": "https://talkdata.herokuapp.com/",
+    "Access-Control-Allow-Methods": "GET, DELETE, HEAD, OPTIONS",
   },
 });
 
