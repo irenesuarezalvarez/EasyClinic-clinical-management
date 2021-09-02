9aab670556b8b11e706e6135c86ff8def93afbf1
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-09-02 23:12:52 +0200

    Set credentials to false axios api

--- src/utils/AxiosApi.js
+++ src/utils/AxiosApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosApi = axios.create({
   baseURL: process.env.REACT_APP_APIURL,
-  withCredentials: true,
+  withCredentials: false,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Access-Control-Allow-Headers': '*',
