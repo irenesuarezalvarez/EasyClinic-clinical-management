77ad7454c48c257b7c856f5fb9dc19eb320662b3
Irene Suarez <irenesuarezalvarez@gmail.com>
2021-09-03 23:00:02 +0200

    'Axios api base url bachnged and headers removed'

--- src/utils/AxiosApi.js
+++ src/utils/AxiosApi.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const axiosApi = axios.create({
   baseURL: process.env.REACT_APP_APIURL,
   withCredentials: true,
-  timeout: 10000,
-  headers: {
-    'Content-Type': 'application/json'
-  },
 });
 
 export default axiosApi;
 
 /* process.env.REACT_APP_APIURL // "http://localhost:5000" */
+/* timeout: 10000,
+headers: {
+  'Content-Type': 'application/json'
+}, */
